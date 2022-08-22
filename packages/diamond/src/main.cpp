bbf2e5226d21daa219cc667e92f0c1723e392fba
ramblehead <v.rybynok@gmail.com>
2022-08-22 12:53:06 +0100

    Checkpoint

--- packages/diamond/src/main.cpp
+++ packages/diamond/src/main.cpp
@@ -1,40 +1,43 @@
 // Hey Emacs, this is -*- coding: utf-8 -*-
 
+#include <unicode/uchar.h>
+
 #include <codecvt>
 #include <iostream>
 #include <locale>
 #include <range/v3/to_container.hpp>
 #include <range/v3/view/transform.hpp>
 
-auto main(int /*argc*/, char* /*argv*/[]) -> int {
-  // std::vector<int> numbers{1, 2, 3};
+inline auto toUpper(char32_t value) -> char32_t {
+  return u_toupper(value);
+}
 
-  // auto halved = numbers
-  //               // Divide each integer by 2, converting it into a double
-  //               | ranges::views::transform(
-  //                   [](const int& num) { return num / 2.0; });
+auto toUpper(const std::u32string& value) -> std::u32string {
+  return value | ranges::views::transform([](auto letter) {
+           return u_toupper(letter);
+         }) |
+         ranges::to<std::u32string>();
+};
 
-  // std::cout << halved << '\n';
-
-  // std::cout << "Hello World!" << std::endl;
-
-  const std::u32string firstIn{U"а"};
-  const std::u32string lastIn{U"д"};
-
-  const auto first = lastIn | ranges::views::transform([](auto letter) {
-                       return std::toupper(letter);
-                     }) |
-                     ranges::to<std::u32string>();
-
-  // const auto first{std::toupper(firstIn)};
-  // const auto last{std::toupper(lastIn)};
-
-  // std::locale loc(std::locale(), new std::codecvt_utf8<char32_t>);
-  // std::cout.imbue(loc);
-
+inline auto operator<<(std::ostream& stream, const std::u32string& value)
+  -> std::ostream& {
   std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+  stream << conv.to_bytes(value);
+  return stream;
+}
 
-  std::cout << "First letter: " << conv.to_bytes(first)
+inline auto operator<<(std::ostream& stream, const char32_t value)
+  -> std::ostream& {
+  std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+  stream << conv.to_bytes(value);
+  return stream;
+}
+
+auto main(int /*argc*/, char* /*argv*/[]) -> int {
+  const char32_t firstIn{U'а'};
+  const char32_t lastIn{U'д'};
+
+  std::cout << "First letter: " << toUpper(firstIn)
             << "\n"
             // << "Last letter: " << conv.to_bytes(lastIn) << "\n"
             << std::endl;
