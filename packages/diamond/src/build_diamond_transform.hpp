78c541e8624a9475a02cd3ed482086b203ed66c8
ramblehead <v.rybynok@gmail.com>
2022-08-24 10:41:49 +0100

    Checkpoint

--- packages/diamond/src/build_diamond_transform.hpp
+++ packages/diamond/src/build_diamond_transform.hpp
@@ -31,15 +31,15 @@ buildDiamond(char32_t first, char32_t last, char32_t fill)
   auto rng = views::iota(0) | views::take(length);
   auto gen = views::transform(rng, [&](char32_t idx) {
     if (idx == 0) {
-      const auto edge = u32string(lastIndex, fill);
+      const auto side = u32string(lastIndex, fill);
       const auto mid = u32string{first};
-      return views::concat(edge, mid, edge) | to<u32string>();
+      return views::concat(side, mid, side) | to<u32string>();
     }
 
     if (idx == lastIndex) {
-      const u32string edge{last};
+      const u32string side{last};
       const u32string mid(lastIndex * 2 - 1, fill);
-      return views::concat(edge, mid, edge) | to<u32string>();
+      return views::concat(side, mid, side) | to<u32string>();
     }
 
     const std::array slices{
