daa5e5a6cc5ad6605a8195b80ce353179afd23ff
ramblehead <v.rybynok@gmail.com>
2022-08-24 17:39:55 +0100

    Checkpoint

--- packages/diamond/src/build_diamond_transform_no_if.hpp
+++ packages/diamond/src/build_diamond_transform_no_if.hpp
@@ -8,18 +8,11 @@
 
 namespace rh_ogorod::diamond_dojo {
 
-using std::u32string;
-using std::vector;
-
-using ranges::end;
-using ranges::to;
-
-namespace views = ranges::views;
-
 [[nodiscard]] inline auto
 buildDiamond(char32_t first, char32_t last, char32_t fill)
-    -> vector<u32string> {
+    -> std::vector<std::u32string> {
   using U32strings = decltype(buildDiamond(first, last, fill));
+  namespace views = ranges::views;
 
   if (first > last) {
     std::swap(first, last);
@@ -30,19 +23,19 @@ buildDiamond(char32_t first, char32_t last, char32_t fill)
   const auto indices = views::iota(0) | views::take(length);
   const auto lines = views::transform(indices, [&](char32_t idx) {
     const std::array slices{
-        u32string(length - idx - 1, fill),
-        u32string{first + idx},
-        u32string(idx, fill)};
+        std::u32string(length - idx - 1, fill),
+        std::u32string{first + idx},
+        std::u32string(idx, fill)};
 
     const auto left = slices | views::join;
     auto right = left | views::reverse | views::drop(1);
 
-    return views::concat(left, right) | to<u32string>();
+    return views::concat(left, right) | ranges::to<std::u32string>();
   });
 
   const auto top = lines | views::take(length);
-  const auto bottom = top | views::slice(0, end - 1) | views::reverse;
-  return views::concat(top, bottom) | to<U32strings>();
+  const auto bottom = top | views::reverse | views::drop(1);
+  return views::concat(top, bottom) | ranges::to<U32strings>();
 }
 
 } // namespace rh_ogorod::diamond_dojo
